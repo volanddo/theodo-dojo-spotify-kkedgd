e4ecff39ca9771bfe6467db866a6e096e5c5ec9b
volanddo <89385228+volanddo@users.noreply.github.com>
2023-09-19 14:46:08 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
+
+const [trackIndex, setTrackIndex] = useState(0);
 
 const App = () => {
   return (
@@ -11,9 +22,15 @@ const App = () => {
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
+      <audio src={trackUrls[0]} autoPlay controls />
+      <button onClick={goToNextTrack}>Next track</button>
       <div className="App-buttons"></div>
     </div>
   );
+};
+
+const goToNextTrack = () => {
+  setTrackIndex(trackIndex + 1);
 };
 
 export default App;
